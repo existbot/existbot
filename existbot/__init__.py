ca24ec03de6a45bae0186e01dc863f777287c755
itslukej <me@lukej.me>
2016-03-11 21:05:10 +0000

    Update __init__.py

--- existbot/__init__.py
+++ existbot/__init__.py
@@ -1,2 +1,52 @@
 #ExistBot 2016
-#Created by zz & Bowserinator ( freenode )
+#Created by zz & Bowserinator (freenode)
+
+import socket
+import ssl as securesl
+from time import sleep
+
+def send(data):
+    print("[SEND] {}".format(data))
+    irc.send("{}\r\n".format(data).encode("UTF-8"))
+
+def recv():
+    part = ""
+    data = ""
+    while not part.endswith("\r\n"):
+        part = irc.recv(2048)
+        part = part.decode()
+        data += part
+    data = data.splitlines()
+    return data
+
+def printrecv():
+    ircmsg = recv()
+    for line in ircmsg:
+        print("[RECV] {}".format(line))
+    return ircmsg
+
+def run(host=None, port=6667, ssl=False, nick=None, ident="ExistBot", realname="ExistBot", channels=["#ExistBot"]):
+    global irc
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if ssl == True:
+        irc = securesl.wrap_socket(sock)
+    else:
+        irc = sock
+    if host == None:
+        raise NameError('host')
+    else:
+        irc.connect((host, port))
+    if nick == None:
+        raise NameError('nick')
+    else:
+        send("NICK {}".format(nick))
+    send("USER {} * * :{}".format(ident, realname))
+    for channel in channels:
+        send("JOIN {}".format(channel))
+        sleep(1)
+    while True:
+        printrecv()
+
+#Testing just for now
+run(port=6697, ssl=True, host="irc.freenode.net", nick="zz|test", channels=["##BWbellairs-bots"])
+
