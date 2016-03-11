b8fe04c878a5344b4359a5e723cb9c1e6f20328e
itslukej <me@lukej.me>
2016-03-11 23:13:50 +0000

    c9 sesh

--- ezzybot/__init__.py
+++ ezzybot/__init__.py
@@ -4,7 +4,7 @@
 import socket
 import ssl as securesl
 from time import sleep
-import thingdb
+import traceback
 
 def send(data):
     print("[SEND) {}".format(data))
@@ -15,7 +15,7 @@ def recv():
     data = ""
     while not part.endswith("\r\n"):
         part = irc.recv(2048)
-        part = part.decode()
+        part = part.encode('utf8')
         data += part
     data = data.splitlines()
     return data
@@ -26,7 +26,13 @@ def printrecv():
         print("[RECV) {}".format(line))
     return ircmsg
     
+commands = {}
+    
+def assign(function, help_text, commandname, prefix="!"):
+    commands[prefix+commandname] = {"function": function, "help": help_text, "prefix": prefix, "commandname": commandname, "fullcommand": prefix+commandname}
 
+def sendmsg(chan, msg):
+    irc.send("PRIVMSG {0} :{1}\n".format(chan, msg))#.encode('utf-8'))
     
 
 def run(config={}):
@@ -41,7 +47,7 @@ def run(config={}):
     analytics = config.get("analytics") or True
     quit_message = config.get("quit_message") or "EzzyBot: a simple python framework for IRC bots."
 
-    if Analytics == True:
+    if analytics == True:
         channels.append("#EzzyBot")
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,6 +55,7 @@ def run(config={}):
         irc = securesl.wrap_socket(sock)
     else:
         irc = sock
+    print "[SEND) Connect {}:{}".format(host, port)
     irc.connect((host, port))
     send("NICK {}".format(nick))
     send("USER {} * * :{}".format(ident, realname))
@@ -57,14 +64,29 @@ def run(config={}):
         while True:
             msg = printrecv()
             for irc_msg in msg:
-                irc_msg = ircmsg.strip(":")
+                irc_msg = irc_msg.strip(":")
                 t = irc_msg.split()
-                
+                #:zz!Zc-zz@mixtape.zzirc.xyz PRIVMSG #ezzybot :test
                 if t[0] == "PING":
                     send("PONG {}".format(" ".join(t[1:])))
+                elif t[1] == "PRIVMSG":
+                    ircmsg = irc_msg
+                    nick = ircmsg.split("!")[0][1:]
+                    channel = ircmsg.split(' PRIVMSG ')[-1].split(' :')[0]
+                    hostmask = ircmsg.split(" PRIVMSG ")[0].split("@")[1].replace(" ","")
+                    
+                    command = ircmsg.split(" :",1)[1]
+                    if command in commands.keys():
+                        output =commands[command]['function']()
+                        if output != None:
+                            sendmsg(channel,output)
+                    
+                # if len(t) >= 4:
+                #     command = t[3].replace(":", "", 1)
+                #     if command in commands.keys():
+                #         print commands[command]['function']()
     except KeyboardInterrupt:
         send("QUIT :{}".format(quit_message))
         irc.close()
-
-#Testing just for now
-run({"channels":["##bwbellairs-bots"]})
+    except:
+        traceback.print_exc()
