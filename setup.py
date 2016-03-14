391a3d8193172cf1e291dfb36536b3ed809b7dd9
itslukej <me@lukej.me>
2016-03-14 23:31:18 +0000

    Enough stuff aded

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='ezzybot',
-      version='1.0.2',
+      version='1.1.0',
       description="Python IRC framework",
       url='https://ezzybot.zzirc.xyz',
       author='EzzyBot team',
