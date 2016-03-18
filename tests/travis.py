aeca488b2f8ab7907de55750581ab0835e63958d
itslukej <me@lukej.me>
2016-03-18 17:46:52 +0000

    better script

--- tests/travis.py
+++ tests/travis.py
@@ -1,11 +1,15 @@
 #travis.py
 import ezzybot, threading, time
 mybot = ezzybot.bot()
+config = {"channels":["#ezzybot"], "nick": "EzzyBot-Travis"}
 def runbot():
-    mybot.run({"channels":["#ezzybot"], "nick": "EzzyBot-Travis"})
+    mybot.run(config)
 
 t = threading.Thread(target=runbot)
 t.daemon=True
 t.start()
-time.sleep(30)
+time.sleep(20)
+mybot.irc.send("PRIVMSG #ezzybot :{}Travis Testing script successful!\r\n".format(mybot.colours.CYAN))
+mybot.irc.send("PART #ezzybot\r\n")
+time.sleep(1)
 exit()
