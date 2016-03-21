a558a3191a939a595ea884257f6b4e3845745efc
itslukej <me@lukej.me>
2016-03-21 21:41:10 +0000

    Update travis.py

--- tests/travis.py
+++ tests/travis.py
@@ -1,7 +1,7 @@
 #travis.py
 import ezzybot, threading, time
 mybot = ezzybot.bot()
-config = {"channels":["#ezzybot"], "nick": "EzzyBot-Travis"}
+config = {"channels":["#ezzybot-debug"], "nick": "EzzyBot-Travis"}
 def runbot():
     mybot.run(config)
 
@@ -9,7 +9,7 @@ t = threading.Thread(target=runbot)
 t.daemon=True
 t.start()
 time.sleep(20)
-mybot.irc.send("PRIVMSG #ezzybot :{0}Travis Testing script successful!\r\n".format(mybot.colours.CYAN))
-mybot.irc.send("PART #ezzybot\r\n")
+mybot.irc.send("PRIVMSG #ezzybot-debug :{0}Travis Testing script successful!\r\n".format(mybot.colours.CYAN))
+mybot.irc.send("PART #ezzybot-debug\r\n")
 time.sleep(1)
 exit()
