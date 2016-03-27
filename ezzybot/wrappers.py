b61a70a11e182e7abf1883956b9ddd2d8f5e2d3d
Bowserinator <bowserinator@gmail.com>
2016-03-26 20:16:22 -0400

    More functions from AegisServer2 :D

--- ezzybot/wrappers.py
+++ ezzybot/wrappers.py
@@ -10,7 +10,6 @@ log=None
 def specify(local_log):
     global log
     log = local_log
-
 class permissions_class(object):
     def __init__(self, permissions):
         self.permissions = permissions # {"admin": "zz!*@*"}
@@ -58,18 +57,54 @@ class connection_wrapper(object):
         self.db = thingdb.thing
         self.bot=bot_class
     def send(self, raw):
-        if self.flood_protection==False:
-            self.irc.send("{0}\r\n".format(raw))#.encode("UTF-8"))
+        if not self.flood_protection:
+            self.irc.send("{}\r\n".format(raw))#.encode("UTF-8"))
         else:
-            flood_protect.queue_add(self.irc, "{0}\r\n".format(raw))#.encode("UTF-8"))
+            flood_protect.queue_add(self.irc, "{}\r\n".format(raw))#.encode("UTF-8"))
     def msg(self, channel, message):
         #self.send("PRIVMSG {} :{}".format(channel, message))
         MSGLEN = 459 - 10 - len(channel)
         message_byte_count = sys.getsizeof(message)-37
         strings = [message[i:i+MSGLEN] for i in range(0, message_byte_count, MSGLEN)]
         for message in strings:
-            self.send("PRIVMSG {0} :{1}".format(channel, message))
+            self.send("PRIVMSG {} :{}".format(channel, message))
     def notice(self, user, message):
-        self.send("NOTICE {0} :{1}".format(user, message))
+        self.send("NOTICE {} :{}".format(user, message))
     def quit(self, message=""):
         self.send("QUIT :"+message)
+    def ctcp(self, user, message):
+        self.send("PRIVMSG {} :\x01{}\x01\x01".format(user, message))
+        
+    def ping(self): 
+        self.irc.send("PONG :pingis\n".encode('utf-8'))
+    def partchan(self,chan):
+        self.irc.send("PART {0}\n".format(chan).encode('utf-8'))
+    def changenick(self,nick):
+        self.irc.send("NICK {0}\n".format(nick).encode('utf-8'))
+    def sendmsg(self,chan, msg):
+        self.irc.send("PRIVMSG {0} :{1}\n".format(chan, msg))#.encode('utf-8'))
+    def joinchan(self,chan):  
+        self.irc.send("JOIN {0}\n".format(chan).encode('utf-8'))
+    def action(self,channel,message):
+        self.sendmsg(channel,"\x01ACTION " + message + "\x01")
+    def kickuser(self,channel,user,message):
+        user = user.replace(" ","").replace(":","")
+        self.irc.send("KICK " + channel + " " + user+ " :" + message +"\r\n")
+    def opnick(self,channel,nick):
+        self.irc.send("MODE {0} +o {1}\n".format(channel,nick).encode('utf-8'))
+    def deopnick(self,channel,nick):
+        self.irc.send("MODE {0} -o {1}\n".format(channel,nick).encode('utf-8'))
+    def ban(self,channel,nick):
+        self.irc.send("MODE {0} +b {1}\n".format(channel,nick).encode('utf-8'))
+    def unban(self,channel,nick):
+        self.irc.send("MODE {0} -b {1}\n".format(channel,nick).encode('utf-8'))
+    def stab(self,channel,nick):
+        self.irc.send("MODE {0} +q {1}\n".format(channel,nick).encode('utf-8'))
+    def unstab(self,channel,nick):
+        self.irc.send("MODE {0} -q {1}\n".format(channel,nick).encode('utf-8'))
+    def unvoice(self,channel,nick):
+        self.irc.send("MODE {0} -v {1}\n".format(channel,nick).encode('utf-8'))
+    def voice(self,channel,nick):
+        self.irc.send("MODE {0} +v {1}\n".format(channel,nick).encode('utf-8'))
+    def setMode(self,channel,nick,mode):
+        self.irc.send("MODE {0} {1} {2}\n".format(channel,mode,nick).encode('utf-8'))
