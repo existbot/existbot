f5ac573831449aac754b56d725173c63a7886087
itslukej <me@lukej.me>
2016-03-27 22:26:35 +0100

    more changes [ci skip]

--- ezzybot/util/hook.py
+++ ezzybot/util/hook.py
@@ -14,6 +14,7 @@ def command(arg=None, **kwargs):
         args.setdefault('help', inspect.getdoc(func))
         args.setdefault('prefix', '!')
         args.setdefault('perms', 'all')
+        args.setdefault('requires', [])
         args.update(kwargs)
         args.setdefault('fullcommand', args["prefix"]+args["commandname"])
         commands[args["prefix"]+args["commandname"]] = args
@@ -26,6 +27,7 @@ def regex(arg=None, **kwargs):
     args = {}
     def command_wrapper(func):
         args.setdefault('function', func)
+        args.setdefault('requires', [])
         args.update(kwargs)
         regexs.append(args)
         return func
@@ -38,9 +40,17 @@ def trigger(arg=None, **kwargs):
     def command_wrapper(func):
         args.setdefault('function', func)
         args.setdefault('trigger', 'PRIVMSG')
+        args.setdefault('requires', [])
         args.update(kwargs)
-        tirggers.append(args)
+        triggers.append(args)
         return func
     if callable(arg):
         return command_wrapper(arg)
     return command_wrapper
+
+#@command
+#def moo(conn, info): #MUTICOLORED MOOOOOOOOOOOS nice
+#    """Returns moo<x>"""
+#    return "\x02\x032mo{}".format("\x03{0}o".format(random.randint(1,15)) * random.randint(1, 25))
+    
+#print commands
