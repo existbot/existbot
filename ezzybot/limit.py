2d94f5b8c7930a56fe02bc1b66eb49c4d3d0b5a8
itslukej <me@lukej.me>
2016-04-01 15:34:09 +0100

    admin override [ci skip]

--- ezzybot/limit.py
+++ ezzybot/limit.py
@@ -1,13 +1,19 @@
 from util import bucket as tokenbucket
+import wrappers
 
 
 class Limit(object):
-    def __init__(self, command_limiting_initial_tokens=20, command_limiting_message_cost=4, command_limiting_restore_rate=0.13):
+    def __init__(self, command_limiting_initial_tokens, command_limiting_message_cost, command_limiting_restore_rate, override, permissions):
         self.command_limiting_initial_tokens = command_limiting_initial_tokens
         self.command_limiting_message_cost = command_limiting_message_cost
         self.command_limiting_restore_rate = command_limiting_restore_rate
         self.buckets = {}
+        self.permissions = wrappers.permissions_class(permissions)
+        self.override = override
     def command_limiter(self, info):
+        #Check if admin/whatever specified
+        if self.permissions.check(self.override, info.mask):
+            return True
         if info.nick not in self.buckets:
             bucket = tokenbucket.TokenBucket(self.command_limiting_initial_tokens, self.command_limiting_restore_rate)
             self.buckets[info.nick] = bucket
