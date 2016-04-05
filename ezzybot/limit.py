29ff0e24c65df04b1c28cdd34e3f1c7300b9271f
ItsLukeJames <me@lukej.me>
2016-04-05 01:41:14 +0100

    More docstrings

--- ezzybot/limit.py
+++ ezzybot/limit.py
@@ -4,6 +4,17 @@ import wrappers
 
 class Limit(object):
     def __init__(self, command_limiting_initial_tokens, command_limiting_message_cost, command_limiting_restore_rate, override, permissions):
+        """limit(20, 4, 0.13, ["admin"], {"admin": "user!*@*"})
+        
+        Limits the use of commands
+        
+        Arguments:
+            command_limiting_initial_tokens {Integer} -- Initial tokens for tokenbucket
+            command_limiting_message_cost {Integer} -- Message cost for tokenbucket
+            command_limiting_restore_rate {Integer} -- Restore rate for token bucket
+            override {List} -- List of permissions to override the limit
+            permissions {Dict} -- All of the bots permissions.
+        """
         self.command_limiting_initial_tokens = command_limiting_initial_tokens
         self.command_limiting_message_cost = command_limiting_message_cost
         self.command_limiting_restore_rate = command_limiting_restore_rate
