eaa6ede7907cab58084965a1f54cae4403140c67
itslukej <me@lukej.me>
2016-04-17 21:53:54 +0100

    New repl class

--- ezzybot/util/repl.py
+++ ezzybot/util/repl.py
@@ -1,35 +1,20 @@
-import code, sys, colours #Kinda made by doge, thanks doge.
-
-colours = colours.colours()
+import code, sys
 
 class Repl(code.InteractiveConsole):
-    def __init__(self, conn):
-        code.InteractiveConsole.__init__(self, {"conn": conn})
-        self.conn = conn
-        self.channel = None
-        self.buf = ""
-
+    def __init__(self, items={}):
+        code.InteractiveConsole.__init__(self, items)
+        self._buffer = ""
     def write(self, data):
-        self.buf += str(data)
-
-    def flush(self):
-        lines=self.buf.split("\n")
-        for line in lines:
-            if self.channel is not None:
-                if line != "":
-                    self.conn.msg(self.channel, "{}| {}".format(colours.LIGHTGREEN, line))
-        self.buf = ""
-
-    def run(self, channel, code):
-        self.channel = channel
+        self._buffer += str(data)
+    def run(self, data):
         sys.stdout = self
-        self.push(code)
+        self.push(data)
         sys.stdout = sys.__stdout__
-        self.flush()
-
+        result = self._buffer
+        self._buffer = ""
+        return result
     def showtraceback(self):
         exc_type, value, lasttb = sys.exc_info()
-        self.conn.msg(self.channel, colours.RED+"{0}: {1}".format(exc_type.__name__, value))
-
+        return "{0}: {1}".format(exc_type.__name__, value)
     def showsyntaxerror(self, filename):
-        self.showtraceback()
+        return self.showtraceback()
