0a80529123fbfbfd42a7816777769d04481d68c1
Bradley Shaw <admin@indigotiger.me>
2016-04-19 20:08:08 +0100

    remove some unneeded encodes

--- ezzybot/wrappers.py
+++ ezzybot/wrappers.py
@@ -110,33 +110,33 @@ class connection_wrapper(object):
         flood_protect.__init__()
         return str(size)
     def part(self,chan):
-        self.send("PART {0}".format(chan).encode('utf-8'))
+        self.send("PART {0}".format(chan))
     def nick(self,nick):
-        self.send("NICK {0}".format(nick).encode('utf-8'))
+        self.send("NICK {0}".format(nick))
     def join(self,chan):  
-        self.send("JOIN {0}".format(chan).encode('utf-8'))
+        self.send("JOIN {0}".format(chan))
     def invite(self, chan, user):
-        self.send("INVITE {} {}".format(user, chan).encode("utf-8"))
+        self.send("INVITE {} {}".format(user, chan))
     def action(self,channel,message):
         self.sendmsg(channel,"\x01ACTION " + message + "\x01")
     def kick(self,channel,user,message):
         user = user.replace(" ","").replace(":","")
         self.send("KICK " + channel + " " + user+ " :" + message)
     def op(self,channel,nick):
-        self.send("MODE {0} +o {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} +o {1}".format(channel,nick))
     def deop(self,channel,nick):
-        self.send("MODE {0} -o {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} -o {1}".format(channel,nick))
     def ban(self,channel,nick):
-        self.send("MODE {0} +b {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} +b {1}".format(channel,nick))
     def unban(self,channel,nick):
-        self.send("MODE {0} -b {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} -b {1}".format(channel,nick))
     def quiet(self,channel,nick):
-        self.send("MODE {0} +q {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} +q {1}".format(channel,nick))
     def unquiet(self,channel,nick):
-        self.send("MODE {0} -q {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} -q {1}".format(channel,nick))
     def unvoice(self,channel,nick):
-        self.send("MODE {0} -v {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} -v {1}".format(channel,nick))
     def voice(self,channel,nick):
-        self.send("MODE {0} +v {1}".format(channel,nick).encode('utf-8'))
+        self.send("MODE {0} +v {1}".format(channel,nick))
     def mode(self,channel,nick,mode):
-        self.send("MODE {0} {1} {2}".format(channel,mode,nick).encode('utf-8'))
+        self.send("MODE {0} {1} {2}".format(channel,mode,nick))
