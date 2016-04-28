328beaba856d3a112d71eade13fea6b6eedeb16f
itslukej <me@lukej.me>
2016-04-28 18:20:47 +0100

    Change log channel

--- tests/travis.py
+++ tests/travis.py
@@ -1,7 +1,7 @@
 #travis.py
 import ezzybot, threading, time, sys
 mybot = ezzybot.bot()
-config = {"channels":[], "nick": "EzzyBot-CI-"+str(sys.version.split(" ")[0].replace(".", "-"))}
+config = {"channels":[], "nick": "EzzyBot-CI-"+str(sys.version.split(" ")[0].replace(".", "-")), "log_channel": "#ezzybot"}
 t = threading.Thread(target=mybot.run, args=(config,))
 t.daemon=True
 t.start()
