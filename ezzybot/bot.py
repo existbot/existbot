bd8ffcf9a6d8c7eafa2cde5b9950664d25153626
itslukej <me@lukej.me>
2016-05-01 22:52:15 +0100

    Change that back

--- ezzybot/bot.py
+++ ezzybot/bot.py
@@ -57,11 +57,11 @@ class bot(object):
                     plugin = importlib.import_module("plugins."+i.split("/")[-2])
                     plugins["plugins."+i.split("/")[-2]] = plugin
         self.defaults()
-        hook.events = []
+        #hook.events = []
         for pluginname, plugin in plugins.items():
             globals()[pluginname] = reload(plugin)
         self.log.debug("Plugins sucessfully imported", info.channel)
-        self.events = hook.events
+        self.events = self.events+hook.events
         self.log.debug("Plugins sucessfully added to list", info.channel)
     reload_bot._commandname = "reload"
     reload_bot._prefix = "!"
