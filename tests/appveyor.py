3f8728221a71251e73128e0457c949b5a0918c92
Bradley Shaw <admin@indigotiger.me>
2016-06-04 13:48:08 +0100

    Give AppVeyor it's own test

---
+++ tests/appveyor.py
@@ -0,0 +1,11 @@
+#travis.py
+import ezzybot, threading, time, sys
+mybot = ezzybot.bot()
+config = {"channels":[], "nick": "EzzyBot-AV-"+str(sys.version.split(" ")[0].replace(".", "-")), "log_channel": "#ezzybot"}
+t = threading.Thread(target=mybot.run, args=(config,))
+t.daemon=True
+t.start()
+time.sleep(20)
+mybot.log.debug("Testing script successful")
+time.sleep(1)
+exit()
